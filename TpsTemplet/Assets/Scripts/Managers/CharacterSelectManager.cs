44273305a2a63692855ab358d14b3bf86303aca0
civalwar114 <sly114@naver.com>
2025-03-23 22:00:15 +0900

    파일 정리 중

--- TpsTemplet/Assets/Scripts/Managers/CharacterSelectManager.cs
+++ TpsTemplet/Assets/Scripts/Managers/CharacterSelectManager.cs
@@ -7,6 +7,7 @@ public class CharacterSelectManager : MonoBehaviour
 
     private TextMeshProUGUI characterName;
     private Image characterImage;
+    private string selectedCharacter;       //제이슨 값을 가져오기 위해 선택한 캐릭터 명을 담을 변수
 
     private void Awake()
     {
@@ -17,24 +18,34 @@ public class CharacterSelectManager : MonoBehaviour
     public void OnKazusaClicked()
     {
         Debug.Log("Kazusa");
+        selectedCharacter = "Kazusa";
         characterName.text = "카즈사";
     }
 
     public void OnMariClicked()
     {
         Debug.Log("Mari");
+        selectedCharacter = "Mari";
         characterName.text = "마리";
     }
 
     public void OnNikoClicked()
     {
         Debug.Log("Niko");
+        selectedCharacter = "Niko";
         characterName.text = "니코";
     }
 
     public void OnShirokoClicked()
     {
         Debug.Log("Shiroko");
+        selectedCharacter = "Shiroko";
         characterName.text = "시로코";
+    }
+
+
+    public void OnCharacterConfirm()
+    {
+
     }
 }
