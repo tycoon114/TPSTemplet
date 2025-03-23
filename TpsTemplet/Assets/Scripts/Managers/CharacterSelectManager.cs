98643b0afaa16e49ef9bf68ea94aa96585fb46a0
civalwar114 <sly114@naver.com>
2025-03-23 21:52:24 +0900

    캐릭터 선택창 구현중

--- TpsTemplet/Assets/Scripts/Managers/CharacterSelectManager.cs
+++ TpsTemplet/Assets/Scripts/Managers/CharacterSelectManager.cs
@@ -1,16 +1,40 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelectManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private TextMeshProUGUI characterName;
+    private Image characterImage;
+
+    private void Awake()
     {
-        
+        characterName = GameObject.Find("CharacterName").GetComponent<TextMeshProUGUI>();
+        characterImage = GameObject.Find("CharacterImage").GetComponent<Image>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnKazusaClicked()
     {
-        
+        Debug.Log("Kazusa");
+        characterName.text = "카즈사";
+    }
+
+    public void OnMariClicked()
+    {
+        Debug.Log("Mari");
+        characterName.text = "마리";
+    }
+
+    public void OnNikoClicked()
+    {
+        Debug.Log("Niko");
+        characterName.text = "니코";
+    }
+
+    public void OnShirokoClicked()
+    {
+        Debug.Log("Shiroko");
+        characterName.text = "시로코";
     }
 }
