65f1b842ff0a51d29023318f0c99219c19fd266f
civalwar114 <sly114@naver.com>
2025-04-04 00:02:29 +0900

    주석 추가

--- TpsTemplet/Assets/Scripts/Managers/PlayerManager.cs
+++ TpsTemplet/Assets/Scripts/Managers/PlayerManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.InputSystem.XR;
 using UnityEngine.SceneManagement;
 
 //플레이어의 체력, 사망, 부활, 피격 등 상태에 관한 매니저를 다룰 코드
+//발소리에 관한 것도 이곳에서 처리함
 //Json 형태의 데이터를 불러오는 식으로 만들 예정
 //palyer 빈 오브젝트가 아닌 캐릭터 자체에 달아줄것 -> 콜라이더 접근을 위함
 public class PlayerManager : MonoBehaviour
